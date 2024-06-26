eded61f12b9a573f745c98cbc89d5ea565f76b3d
Chris Annunziato <NeonMeteors@gmail.com>
2024-06-26 14:30:23 -0400

    runner on screen

--- start.go
+++ start.go
@@ -1,27 +1,60 @@
 package main
 
 import (
+	"bytes"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
+	"image"
+	_ "image/png"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+const (
+	screenWidth  = 320
+	screenHeight = 240
+	frameOX      = 0
+	frameOY      = 32
+	frameWidth   = 32
+	frameHeight  = 32
+	frameCount   = 8
+)
+
+var (
+	runnerImage *ebiten.Image
+)
+
+type Game struct {
+	count int
+}
 
 func (g *Game) Update() error {
+	g.count++
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Anyone there?")
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64(screenWidth)/2, float64(screenHeight)/2)
+	op.GeoM.Translate(-float64(frameWidth)/2, -float64(frameHeight/2))
+	i := (g.count / 5) % frameCount
+	sx, sy := frameOX+i*frameWidth, frameOY
+	screen.DrawImage(runnerImage.SubImage(image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)).(*ebiten.Image), op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return 320, 240
+
 }
 
 func main() {
+
+	img, _, err := image.Decode(bytes.NewReader(images.Runner_png))
+	if err != nil {
+		log.Fatal(err)
+	}
+	runnerImage = ebiten.NewImageFromImage(img)
+
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
 	if err := ebiten.RunGame(&Game{}); err != nil {
